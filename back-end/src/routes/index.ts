7e1f036d4141ff48bf793aad9639da8006cf6a94
Juliarezende521 <contatojuliagrezende@gmail.com>
2026-09-11 00:49:54 +0000

    (10/09) Inserção do primeiro cliente no banco de dados (teste CRUD)

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,9 +1,17 @@
-import { Request, Response, Router } from 'express'
-const router = Router()
+import { Request, Response, Router } from "express";
+
+import customersRouter from "./customers";
+
+const router = Router();
 
 /* GET home page. */
-router.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!')
-})
 
-export default router
+router.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+/* Rotas de clientes */
+
+router.use("/customers", customersRouter);
+
+export default router;
--- back-end/src/services/customerService.ts
+++ back-end/src/services/customerService.ts
@@ -10,7 +10,7 @@ import type { UpdateCustomerDto } from "../dto/customer/updateCustomerDto.ts";
 import { NotFoundError } from "../errors/NotFoundError";
 
 
-export async function findAll(): Promise<Customer[]> {git add .
+export async function findAll(): Promise<Customer[]> {
  return repository.findAll();
 }
 
